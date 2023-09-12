d7589dada8fc862a8b35336384f91e2b6b27d7b8
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-12 17:19:40 +0545

    another

---
+++ array.rb
@@ -0,0 +1,37 @@
+people = ['Ram', 'Hari', 'Gopal']
+puts people.first
+puts people.last
+puts people.size
+
+puts people.include?("Jack")
+
+puts people.include?("Hari")
+
+puts people.index("Ram")
+puts people.index("Hari")
+puts people.index("Gopal")
+ 
+
+puts people.join
+
+puts people.join(" ")
+
+puts people.push("Bob")
+ people.pop
+puts people
+
+puts people.sample
+
+
+man = ["Jake", "Jake", "Adam", "Mike", "Mike", "Mike"]
+
+puts man.uniq
+
+
+array can contain multiple data type
+
+data = ["this is string" ,10.3 , 10, [true, "hello"], {key:"value"}]
+puts data
+
+person = [["Adam", 21], ["Jake", 23], ["Paul", 30]]
+puts person[0][0]
