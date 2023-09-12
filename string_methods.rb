ba3002ef16b4a6a9563ead63ef4b9781100a4334
saroj <sawroz.adhikari@gmail.com>
2023-09-12 15:40:09 +0545

    my commit

---
+++ string_methods.rb
@@ -0,0 +1,40 @@
+#  Make all letters uppercase
+ puts "i am saroj".upcase
+
+ #Make all letters lowercase4
+ puts "I AM A RUBY PROGRAMMMER".downcase
+
+ # to get the length of the given string 
+ puts " hey i am saroj , I am a Programmmer".length
+
+ # to reverse the string
+ puts " I am reveresd string".reverse
+
+ # to capitalized first letter 
+ puts " my first letter is been capitalized by this piece of code".capitalize
+
+
+ # check if the string exists within string
+ puts "Is this sting contain another string".include?("hii")
+
+
+ #split string into an array of individual character
+ puts "Split this sentence hehe".split("")
+
+ # to convert string into integer
+ puts "1111".to_i
+ 
+ # to convert string into float
+ puts "12121.121231".to_f
+ 
+
+
+ #Assignment for this topic
+ # 1.Check if the character code is exist inside  a string " I love You"
+
+ puts "I love you".include?("code")
+
+ # 2.Find the length of the sting :"How long is this string"
+ puts "How long is this string".length
+
+ puts "hi i am saroj".reverse
