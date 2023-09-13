ef510f4b37b780971723a9498a1e6b982a594f1f
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-13 07:23:15 +0545

    hello

--- array.rb
+++ array.rb
@@ -1,37 +1,38 @@
-people = ['Ram', 'Hari', 'Gopal']
-puts people.first
-puts people.last
-puts people.size
+# people = ['Ram', 'Hari', 'Gopal']
+# puts people.first
+# puts people.last
+# puts people.size
 
-puts people.include?("Jack")
+# puts people.include?("Jack")
 
-puts people.include?("Hari")
+# puts people.include?("Hari")
 
-puts people.index("Ram")
-puts people.index("Hari")
-puts people.index("Gopal")
+# puts people.index("Ram")
+# puts people.index("Hari")
+# puts people.index("Gopal")
  
 
-puts people.join
+# puts people.join
 
-puts people.join(" ")
+# puts people.join(" ")
 
-puts people.push("Bob")
- people.pop
-puts people
+# puts people.push("Bob")
+#  people.pop
+# puts people
 
-puts people.sample
-
-
-man = ["Jake", "Jake", "Adam", "Mike", "Mike", "Mike"]
-
-puts man.uniq
+# puts people.sample
 
 
-array can contain multiple data type
+# man = ["Jake", "Jake", "Adam", "Mike", "Mike", "Mike"]
 
-data = ["this is string" ,10.3 , 10, [true, "hello"], {key:"value"}]
-puts data
+# puts man.uniq
+
+
+# array can contain multiple data type
+
+# data = ["this is string" ,10.3 , 10, [true, "hello"], {key:"value"}]
+# puts data
 
 person = [["Adam", 21], ["Jake", 23], ["Paul", 30]]
-puts person[0][0]
+# puts person[0][0]
+puts person[2][0]
