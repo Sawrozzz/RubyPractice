dabff7678beb70f892eb5545084491a79f1491ea
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-13 21:50:09 +0545

    message

---
+++ conditional_ope.rb
@@ -0,0 +1,19 @@
+# 1. Comparison Operator
+# i.'==' Operator
+# ->for eg. a==b that means return true if a and b are equal
+
+# ii. '!=' Operator
+# -> for eg. a!=b that mean return true if a and b are not equal
+
+# iii. '>' Operator
+# -> for eg. a>b that mean return true if a is greater than b
+
+# iv. '>=' Operator
+# -> for eg. a>=b that mean return true if a is greater than or equal to b
+
+# v. '<' Operator
+# -> for eg. a<b that mean return true if a is less than b 
+
+# vi. '<=' Operator
+# -> for eg. a<=b that men return true if a is less than or equal to b
+
---
+++ first_method.rb
@@ -0,0 +1,23 @@
+#Ruby method is start with def and end with end.
+
+# def hello_world
+#   puts "FIrst code of first method "
+# end
+
+# hello_world
+
+# calling the method is also called invoking the method 
+
+
+# we can pass parameters too. let see in the following code
+# def hi (name)
+#     puts " Hi! Saroj #{name}"
+# end
+# hi "Adhikari"
+
+def bdy(name, age) # multiple parameters  can also be used
+    puts "#{name} age is #{age}"
+end
+bdy("Saroj", 22)
+bdy "Ramesh", 24
+bdy 'Suraj', 33
